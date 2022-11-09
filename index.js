705206b2d6af0353f9e6c31ade7cad2b972da2e1
Bojan Vujic <bojanvjc@gmail.com>
2022-11-09 11:53:46 +0100

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.send( "Pozdrav Boskic!" );
 } );
 
 // start the Express server
